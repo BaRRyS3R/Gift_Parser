fd365f01e9faef83c57017933c015240463f0246
BaRRyS3R <mr.crowley.audio@gmail.com>
2025-08-03 23:31:30 +0300

    Update ton-payments.ts

--- types/ton-payments.ts
+++ types/ton-payments.ts
@@ -62,7 +62,7 @@ export const NANOTONS_PER_TON = BigInt(1000000000);
 
 export const TON_CONFIG = {
     // Корпоративный кошелек для приема платежей
-    CORPORATE_WALLET: "UQDylHxryL2sUApZzEsp9iOGJNpOBZU_88U3ewxZ560hRFLN",
+    CORPORATE_WALLET: "EQDylHxryL2sUApZzEsp9iOGJNpOBZU_88U3ewxZ560hRA8I",
 
     // GetBlock API конфигурация
     GETBLOCK_API_URL: "https://go.getblock.io/cc8c998b532f4fff9e678d879957ca6c",
