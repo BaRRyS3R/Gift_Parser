dc470270b56a9c870b6d44e6f01c8ea0b2a34eb5
BaRRyS3R <mr.crowley.audio@gmail.com>
2025-08-18 23:23:08 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,6 +51,28 @@ export default function RootLayout({
           src="https://telegram.org/js/telegram-web-app.js"
           strategy="beforeInteractive"
         />
+        <Script id="yandex-metrika" strategy="afterInteractive">
+          {`
+            (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){
+                    (m[i].a=m[i].a||[]).push(arguments)
+                };
+                m[i].l=1*new Date();
+                for (var j=0;j<document.scripts.length;j++){
+                    if (document.scripts[j].src===r){return;}
+                }
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],
+                k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+            ym(103798151, "init", {
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true,
+                webvisor:true
+            });
+          `}
+        </Script>
         {/* PWA and safe area support */}
         <meta content="yes" name="mobile-web-app-capable" />
         <meta content="yes" name="apple-mobile-web-app-capable" />
